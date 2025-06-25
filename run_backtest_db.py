778250f481336722a021959ea73cda22d47350e4
taihak-dev <gofurther@gmail.com>
2025-06-25 22:35:26 +0900

    .

--- run_backtest_db.py
+++ run_backtest_db.py
@@ -4,8 +4,8 @@ from manager.simulator_db import simulate_with_db
 
 simulate_with_db(
     market="KRW-DOGE",
-    start="2025-01-01 00:00:00",
-    end="2025-01-01 00:10:00",
+    start="2024-01-01 00:00:00",
+    end="2025-06-01 00:10:00",
     unit_size=5000,
     small_flow_pct=0.04,
     small_flow_units=2,
